d7b082ac5451bf44148701efd586250a0411a8a1
Mathieu Molinengo <molinengo.mathieu@etu.unice.fr>
2015-05-17 19:12:37 +0200

    tag or not

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,52 +8,52 @@
  *
  * Main module of the application.
  */
-var app=angular
-  .module('showcaseApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/add', {
-        templateUrl: 'views/add.html',
-        controller: 'AddCtrl'
-      })
-      .when('/delete', {
-        templateUrl: 'views/delete.html',
-        controller: 'DeleteCtrl'
-      })
-        .when('/:userId/affiche',{
-        templateUrl:'views/affiche.html',
-        controller:'AfficheCtrl'
-        })
-      .when('/edit', {
-        templateUrl: 'views/edit.html',
-        controller: 'EditCtrl'
-      })
-      .when('/projet', {
-          templateUrl: 'views/projet.html',
-          controller: 'ProjectCtrl'
-      })
+var app = angular
+    .module('showcaseApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/add', {
+                templateUrl: 'views/add.html',
+                controller: 'AddCtrl'
+            })
+            .when('/delete', {
+                templateUrl: 'views/delete.html',
+                controller: 'DeleteCtrl'
+            })
+            .when('/:userId/affiche', {
+                templateUrl: 'views/affiche.html',
+                controller: 'AfficheCtrl'
+            })
+            .when('/edit', {
+                templateUrl: 'views/edit.html',
+                controller: 'EditCtrl'
+            })
+            .when('/projet', {
+                templateUrl: 'views/projet.html',
+                controller: 'ProjectCtrl'
+            })
 
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
 
 /* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#nav' })
+$('body').scrollspy({target: '#nav'})
 
 /* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-  $('body,html').animate({scrollTop:0},1000);
+$('.scroll-top').click(function () {
+    $('body,html').animate({scrollTop: 0}, 1000);
 })
 
