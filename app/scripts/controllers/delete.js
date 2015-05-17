534ede7a4ca0ce93f7cbf27184d4dcd07a7ef4b5
Mathieu Molinengo <molinengo.mathieu@etu.unice.fr>
2015-05-17 15:14:18 +0200

    Petit Pastis ici

--- app/scripts/controllers/delete.js
+++ app/scripts/controllers/delete.js
@@ -8,38 +8,74 @@
  * Controller of the showcaseApp
  */
 angular.module('showcaseApp')
-    .controller('DeleteCtrl', ['$scope', '$http', '$routeParams','Suppression','Data', function ($scope, $http, $routeParams,Suppression,Data) {
+    .controller('DeleteCtrl', ['$scope', '$http', '$routeParams', 'Suppression', 'Data', function ($scope, $http, $routeParams, Suppression, Data) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
-        Data.getUsers(0,function(data){$scope.users=data},function(data){})
-        Data.getProjects(0,function(data){$scope.projects=data},function(data){})
-        Data.getRoles(0,function(data){$scope.roles=data},function(data){})
+
+        Data.getUsers(0, function (data) {
+            $scope.users = data
+        }, function (data) {
+        })
+
+        Data.getProjects(0, function (data) {
+            $scope.projects = data
+        }, function (data) {
+        })
+
+        Data.getRoles(0, function (data) {
+            $scope.roles = data
+        }, function (data) {
+        })
+
+        $scope.getUser = function () {
+            Data.getUsers(0, function (data) {
+                $scope.users = data
+            }, function (data) {
+            })
+        }
+        $scope.getProject = function () {
+            Data.getProjects(0, function (data) {
+                $scope.projects = data
+            }, function (data) {
+            })
+        }
+        $scope.getRoles = function () {
+            Data.getRoles(0, function (data) {
+                $scope.roles = data
+            }, function (data) {
+            })
+        }
 
 
-        $scope.deleteUser=function(user){
+        $scope.deleteUser = function (user) {
 
-            Suppression.deleteUser(user,function(data){},function(data){});
+            Suppression.deleteUser(user, function (data) { $scope.getUser();
+            }, function (data) {
+            });
 
         }
 
-        $scope.afficherRole=function(user,project){
+        $scope.afficherRole = function (user, project) {
             Suppression.getRolebyUserandProject(user, project, function (data) {
                     $scope.role = data[0];
                 }
                 , function (data) {
                 });
         }
-        <!-- marche pas -->
-        $scope.deleteRole=function(role){
-            Suppression.deleteRole(role,function(data){},function(data){});
+        $scope.deleteRole = function (role) {
+            Suppression.deleteRole(role, function (data) {$scope.getRoles();
+            }, function (data) {
+            });
         }
 
 
-        $scope.deleteProjet=function(project){
-            Suppression.deleteProjet(project,function(data){},function(data){});
+        $scope.deleteProjet = function (project) {
+            Suppression.deleteProjet(project, function (data) {$scope.getProject();
+            }, function (data) {
+            });
 
         }
 
